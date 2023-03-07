99227c0075eb12deb7dc24f3e2d778a319847580
Ignacio Monzalvo <ignaciomonzalvo@192.168.1.5>
2023-03-06 22:23:50 -0300

    Modify meta

--- src/layout.js
+++ src/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <body>{children}</body>
     </html>
   );
--- src/pages/_documents.js
+++ src/pages/_documents.js
@@ -22,7 +22,7 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="en">
+      <Html lang="es">
         <Head>
           <style
             data-emotion-css={this.props.ids.join(' ')}
