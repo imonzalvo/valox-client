68b43bed70b558acf238dde24b885fb08d7fee0a
Ignacio Monzalvo <ignaciomonzalvo@192.168.1.4>
2023-03-15 19:42:01 -0300

    Add whatsapp

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,11 +6,15 @@ import {
   QueryClientProvider,
 } from "@tanstack/react-query";
 import tw from "twin.macro";
+import { FloatingWhatsApp } from "react-floating-whatsapp";
+
 import { config } from "../lib/react-query-config";
 import GlobalStyles from "./../styles/GlobalStyles";
 import Header from "@/components/header/Header";
 const GlobalContainer = tw.div`flex flex-1 justify-center md:w-full 
                                 sm:w-full lg:w-full px-8 bg-white`;
+
+const whatsAppNumber = process.env.NEXT_PUBLIC_WHATSAPP_NUMBER;
 
 export default function App({ Component, pageProps }) {
   const [queryClient] = useState(() => new QueryClient(config));
@@ -23,6 +27,18 @@ export default function App({ Component, pageProps }) {
         <GlobalContainer>
           <Component {...pageProps} />
         </GlobalContainer>
+        {!!whatsAppNumber && (
+          <div style={{ position: "absolute" }}>
+            <FloatingWhatsApp
+              phoneNumber={whatsAppNumber}
+              statusMessage={"Disponible"}
+              accountName={process.env.REACT_APP_BUSINESS_TITLE}
+              chatMessage={"Buenas! En que podemos ayudarte?"}
+              placeholder={"Escriba su consulta"}
+              avatar="favicon.ico"
+            />
+          </div>
+        )}
       </Hydrate>
     </QueryClientProvider>
   );
