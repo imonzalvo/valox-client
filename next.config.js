34c00facac4b66a25761d49d5385c2d73169143c
Ignacio Monzalvo <ignaciomonzalvo@192.168.1.3>
2023-03-20 02:23:22 -0300

    nextjs config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   poweredByHeader: false,
   images: {
     formats: ['image/avif', 'image/webp'],
-    domains: ['fragrant-sunset-934.fly.dev', 'localhost'],
+    domains: ['valox-admin-test.fly.dev', 'localhost'],
     remotePatterns: [
       {
         protocol: 'https',
