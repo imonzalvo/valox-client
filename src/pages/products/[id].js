413630e7c3cc93222b672d6e9e94525bb1850a15
Ignacio Monzalvo <ignaciomonzalvo@192.168.1.3>
2023-03-22 10:05:41 -0300

    Fix product details

--- src/pages/products/[id].js
+++ src/pages/products/[id].js
@@ -2,7 +2,7 @@ import { useState, useMemo } from "react";
 import tw, { styled } from "twin.macro";
 import { useRouter } from "next/router";
 import { dehydrate, QueryClient } from "@tanstack/react-query";
-
+import ImageGallery from "react-image-gallery";
 
 import * as api from "../../api/products";
 import { useProduct } from "../../hooks/useProduct";
@@ -14,6 +14,7 @@ import ColorPicker from "@/components/products/colors";
 import Topbar from "@/components/products/topBar";
 import ProductImages from "@/components/products/productImages";
 import ProductInfo from "@/components/products/productInfo";
+import Head from "next/head";
 
 const Container = tw.div`mt-12 flex justify-between sm:py-12
 md:flex-row
@@ -26,7 +27,7 @@ const ImageContainer = styled.div`
     height: 500px;
   }
 `;
-
+const ImageContainers = tw.div`w-full md:w-1/2 h-120 flex-1 mb-32`;
 const QuantityPickerContainer = tw.div`my-6`;
 const StyledButton = tw.button`mt-8 cursor-pointer w-full text-sm font-bold tracking-wider bg-transparent 
                               hover:bg-black text-black font-semibold 
@@ -47,6 +48,22 @@ export const getServerSideProps = async (ctx) => {
   };
 };
 
+const getProductImages = (product) =>
+  product.images.map((image) => {
+    const originalImageUrl =
+      image.image.url && image.image.url.includes("localhost")
+        ? `${image.image.url}`
+        : `${process.env.NEXT_PUBLIC_API_URL}${image.image.url}`;
+    const thumbnailUrl =
+      image.image.url && image.image.url.includes("localhost")
+        ? `${image.image.sizes.thumbnail.url}`
+        : `${process.env.NEXT_PUBLIC_API_URL}${image.image.sizes.thumbnail.url}`;
+    return {
+      original: originalImageUrl,
+      thumbnail: thumbnailUrl,
+    };
+  });
+
 const renderButton = (onClick, title) => {
   return (
     <StyledButton onClick={onClick}>
@@ -62,9 +79,16 @@ export default function Product() {
   } = useRouter();
   const [numberOfitems, updateNumberOfItems] = useState(1);
   const { data: product, isFetching } = useProduct(id);
-  const [selectedImage, setSelectedImage] = useState(product.images[0].image.url)
+  const [selectedImage, setSelectedImage] = useState(
+    product.images[0].image.url
+  );
 
   const { cart, addItemToCart, isProductInCart } = useCart();
+
+  const productImages = useMemo(() => {
+    if (!product) return [];
+    return getProductImages(product);
+  }, [product]);
 
   const goToCheckout = () => {
     // addItemToCart({
@@ -92,14 +116,29 @@ export default function Product() {
   }
   return (
     <section class="max-w-6xl pt-8 pb-24 bg-blueGray-100 rounded-b-10xl overflow-hidden">
+      <Head>
+        <title>{`${process.env.NEXT_PUBLIC_BUSINESS_TITLE} | ${product.title}`}</title>
+        <meta name="description" content={product.description} />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <div class="container px-4 mx-auto">
+        {/* // TODO: Flex wrap working badly */}
         <div class="flex flex-wrap -mx-4">
           <Topbar productTitle={product.title} />
-          <ProductImages
-            setSelectedImage={setSelectedImage}
-            images={product.images}
-            selectedImage={selectedImage}
-          />
+          <ImageContainers>
+            <ImageContainer>
+              <ImageGallery
+                items={productImages}
+                showBullets={false}
+                useBrowserFullscreen={false}
+                showPlayButton={false}
+                showNav={false}
+                showFullscreenButton={true}
+                useTranslate3D={false}
+              />
+            </ImageContainer>
+          </ImageContainers>
           <div class="w-full lg:w-1/2 px-4">
             <ProductInfo product={product} />
             {/* <Rating />
